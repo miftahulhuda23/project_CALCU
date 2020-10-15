80aa0796483fa8623cec9ea02cefc6fd9d982491
huda <hudami2301@gmail.com>
2020-10-15 17:27:45 +0700

    fix calculator

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/jquery/dist/jquery.min.js'
+import '../node_modules/popper.js/dist/popper.js'
 
 ReactDOM.render(
   <React.StrictMode>
